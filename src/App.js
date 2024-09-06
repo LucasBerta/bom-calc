eb9d38010b8451822df6e234af2e9eee663d6919
Lucas Berta <lucasberta59@gmail.com>
2024-09-06 12:10:43 +0100

    First version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [poPrice, setPOPrice] = useState(0);
+  const [bomLines, setBomLines] = useState();
+  const [discountForPOPrice, setDiscountForPOPrice] = useState('');
+
+  function calculateDiscount() {
+    const lines = bomLines.split(/\n|\r/g);
+    const matrix = lines.map(line => line.split(/\t/g));
+
+    const indexes = {
+      qty: matrix[0].findIndex(item => item === 'Quantity'),
+      unitPrice: matrix[0].findIndex(item => item === 'Unit Price Excl. VAT'),
+    };
+
+    let totalCost = 0;
+    matrix.forEach((line, index) => {
+      if (index > 0) {
+        totalCost += line[indexes.qty] * line[indexes.unitPrice];
+      }
+    });
+
+    const _discountForPOPrice = ((poPrice / totalCost - 1) * -100).toFixed(5);
+    setDiscountForPOPrice(_discountForPOPrice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <textarea id='bom-lines' rows={20} value={bomLines} onChange={e => setBomLines(e.target.value)}></textarea>
+      <div>
+        <input placeholder='PO Price' type='number' value={poPrice} onChange={e => setPOPrice(e.target.value)} />
+        <button onClick={calculateDiscount}>Calculate Discount</button>
+      </div>
+      {discountForPOPrice && <div style={{ textAlign: 'center', width: '100%' }}>{discountForPOPrice}%</div>}
     </div>
   );
 }
